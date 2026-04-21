f58068032590e5ac185fe4b04a0b19fba7385794
Joy-ConScion <chris.torres2001@outlook.com>
2026-04-21 11:12:07 -0500

    feat: adds line numbers

--- src/main/java/com/pluralsight/Application.java
+++ src/main/java/com/pluralsight/Application.java
@@ -1,4 +1,28 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Application {
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("goldilocks.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+
+        String storyPiece = bufferedReader.readLine();
+int number = 1;
+//++number () vs number++ ()
+
+        while (storyPiece != null) {
+//       TODO: add sout for numbering before printing line
+            System.out.print(number++ + ". ");
+            System.out.println(storyPiece);
+            storyPiece = bufferedReader.readLine();
+
+
+        }
+
+
+    }
 }
